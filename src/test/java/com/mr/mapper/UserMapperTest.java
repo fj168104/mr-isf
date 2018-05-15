854ed8d700c8d0da944c5499aa2ef1910dc50dde
fj168104 <fengj@jxltech.com>
2018-05-16 00:07:37 +0800

    project init !

---
+++ src/test/java/com/mr/mapper/UserMapperTest.java
@@ -0,0 +1,24 @@
+package com.mr.mapper;
+
+import com.mr.BaseTest;
+import com.mr.modules.sys.mapper.UserMapper;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by JK on 2017/1/22.
+ */
+public class UserMapperTest extends BaseTest{
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testFindById(){
+//        User user = userMapper.findById(73L);
+//        if(user != null){
+//            System.out.println(user.getUsername());
+//        }
+//        Assert.assertNotNull(user);
+    }
+}
